12e96f165837fd92f38b207cec00a3b73f0030c8
ReFantasy_labpc <309741309@qq.com>
2022-03-04 10:09:58 +0800

    .

--- example.cpp
+++ example.cpp
@@ -143,18 +143,18 @@ void example()
 
     Functor functor;
 
-    int n = 500;
+    int n = 20;
     Vector x(n);
     //x << -10, 10;
     for (int i = 0; i < x.size(); i++)
     {
         if (i % 2 == 0)
         {
-            x(i) = rand()%1000;
+            x(i) = rand()%100;
         }
         else
         {
-            x(i) = -rand()%1000;
+            x(i) = -rand()%100;
         }
     }
 
@@ -168,7 +168,7 @@ void example()
     options.parameter_line_search_armijo_rho = 1e-3;
     options.parameter_line_search_armijo_t = 3.0;
     options.parameter_line_search_wolfe_rho = 1e-3;
-    options.parameter_line_search_wolfe_sigma = 0.3;
+    options.parameter_line_search_wolfe_sigma = 0.1;
     options.parameter_line_search_wolfe_alpha_max = 200000;
     options.optimized_performance = true;
     options.init_x = x;
